64711ce736e1420d2bfaba764b2eff3eb241edf1
Chucksef <Chucksef@gmail.com>
2020-03-16 11:39:15 -0600

    Added Nav system to load the correct content

---
+++ src/home.js
@@ -0,0 +1,15 @@
+const homeContent = () => {
+    console.log('Displaying Home Content');
+    let a = `<h1>Bosco's Tacos</h1>
+            <img id="logo" src="https://i2.wp.com/gimmedelicious.com/wp-content/uploads/2019/01/Quick-Chicken-Tacos-food-truck-style-4.jpg" alt="Some Tacos @ Bosco's">
+            <h2>ESTD 1973</h2>
+            <h3>Our 'Homerun' Home Recipes</h3>
+            <p>Bosco's might be a strange name for a taco joint. But that hasn't stopped our loyal customers from demanding more than 2000 locations across the United States. We didn't want to do it, but you made it clear that you weren't buying any more tacos from chains fewer than 2000 in number.</p>
+            <h3>Bosco's Big Bold Promise</h3>
+            <p>What is Bosco's Big Bold Promise? Well, according to Bosco di Boscosio, owner and founder of Bosco's Tacos, Bosco's Tacos' "Bosco's Big Bold Promise" is, "Bosco's Tacos will always be the 'Big Beefy Burritos' of the taco world."</p>
+            <p>It's a promise Bosco boasted he would die to uphold, until 1989 when he died feeding his big, beefy burritos to bigger, beefier bears.</p>
+    `
+    return a;
+}
+
+export default homeContent;
---
+++ src/locations.js
@@ -0,0 +1,10 @@
+const locationsContent = () => {
+    console.log('Displaying Locations Content');
+    let a = `<h1>Bosco's Tacos</h1>
+            <h3>Mall Location</h3>
+            <p>In the Flesham Mall between the abandoned Radio Shacks</p>
+    `
+    return a;
+}
+
+export default locationsContent;
