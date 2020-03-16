61561270d3903469c2df87675ce1b0b4f9109bb4
Chucksef <Chucksef@gmail.com>
2020-03-16 17:36:39 -0600

    Basic Look, Style, and Content Present

--- src/index.js
+++ src/index.js
@@ -9,24 +9,35 @@ function showPage(content) {
     target.appendChild(element);
 }
 
-function clearContent() {
+function clearContent(lastElem) {
+    
+    let tabs = document.querySelectorAll(".tab");
+    tabs.forEach(function(tab){
+        tab.className = "tab";
+    })
+    
+    lastElem.className = "tab selected";
     let target = document.querySelector("#content");
     target.innerHTML = "";
+
 }
 
-document.querySelector("#home-tab").addEventListener('click', function(){
-    clearContent();
+document.querySelector("#home-tab").addEventListener('click', function(e){
+    clearContent(e.target);
     showPage(home());
+    
 })
 
-document.querySelector("#menu-tab").addEventListener('click', function(){
-    clearContent();
+document.querySelector("#menu-tab").addEventListener('click', function(e){
+    clearContent(e.target);
     showPage(menu());
+
 })
 
-document.querySelector("#locations-tab").addEventListener('click', function(){
-    clearContent();
+document.querySelector("#locations-tab").addEventListener('click', function(e){
+    clearContent(e.target);
     showPage(locations());
+
 })
 
 showPage(home());
