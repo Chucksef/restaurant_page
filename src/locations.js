ae485e3f36cac66c52daedb348b1cdbb27bea173
Chucksef <Chucksef@gmail.com>
2020-03-16 16:48:17 -0600

    LOTS of formatting finished

--- src/locations.js
+++ src/locations.js
@@ -1,8 +1,9 @@
 const locationsContent = () => {
-    console.log('Displaying Locations Content');
-    let a = `<h1>Bosco's Tacos</h1>
-            <h3>Mall Location</h3>
-            <p>In the Flesham Mall between the abandoned Radio Shacks</p>
+
+    let a = `
+        <h1>Bosco's Tacos</h1>
+        <h3>Mall Location</h3>
+        <p>In the Flesham Mall between the abandoned Radio Shacks</p>
     `
     return a;
 }
--- src/menu.js
+++ src/menu.js
@@ -1,20 +1,21 @@
 const menuContent = () => {
-    console.log('Displaying Menu Content');
-    let a = `<h1>Bosco's Tacos</h1>
-            <h3>Appetizers</h3>
-            <h5>Little Tacos</h5>
-            <p>$8</p>
-            <h5>Taco Platter</h5>
-            <p>$11</p>
-            <h3>Entreés</h3>
-            <h5>Bosco's Bozo Taco</h5>
-            <p>$6</p>
-            <h5>3 Tacos</h5>
-            <p>$12</p>
-            <h5>Big Tacos</h5>
-            <p>$16</p>
-            <h5>Large Taco Platter</h5>
-            <p>$20</p>
+
+    let a = `
+        <h1>Bosco's Tacos</h1>
+        <h3>Appetizers</h3>
+        <h5>Little Tacos</h5>
+        <p>$8</p>
+        <h5>Taco Platter</h5>
+        <p>$11</p>
+        <h3>Entreés</h3>
+        <h5>Bosco's Bozo Taco</h5>
+        <p>$6</p>
+        <h5>3 Tacos</h5>
+        <p>$12</p>
+        <h5>Big Tacos</h5>
+        <p>$16</p>
+        <h5>Large Taco Platter</h5>
+        <p>$20</p>
     `
     return a;
 }
