576847205dcd9b618d6db2ef9bbe87e29d476a2c
Chucksef <Chucksef@gmail.com>
2020-03-16 10:44:52 -0600

    moved all elements to JS

---
+++ src/index.js
@@ -0,0 +1,19 @@
+console.log(`It's running`);
+
+function showPage() {
+    let target = document.querySelector("#content");
+    let element = document.createElement('section')
+    let content = `
+            <h1>Bosco's Tacos</h1>
+            <img id="logo" src="https://i2.wp.com/gimmedelicious.com/wp-content/uploads/2019/01/Quick-Chicken-Tacos-food-truck-style-4.jpg" alt="Some Tacos @ Bosco's">
+            <h2>ESTD 1973</h2>
+            <h3>Our 'Homerun' Home Recipes</h3>
+            <p>Bosco's might be a strange name for a taco joint. But that hasn't stopped our loyal customers from demanding more than 2000 locations across the United States. We didn't want to do it, but you made it clear that you weren't buying any more tacos from chains fewer than 2000 in number.</p>
+            <h3>Bosco's Big Bold Promise</h3>
+            <p>What is Bosco's Big Bold Promise? Well, according to Bosco di Boscosio, owner and founder of Bosco's Tacos, Bosco's Tacos' "Bosco's Big Bold Promise" is, "Bosco's Tacos will always be the 'Big Beefy Burritos' of the taco world."</p>
+            <p>It's a promise Bosco boasted he would die to uphold, until 1989 when he died feeding his big, beefy burritos to bigger, beefier bears.</p>`;
+    element.innerHTML = content;
+    target.appendChild(element);
+}
+
+showPage();
