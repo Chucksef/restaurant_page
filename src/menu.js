46fbda9252ac9345c6605bba450214de32e16d4e
Chucksef <Chucksef@gmail.com>
2020-03-17 13:19:12 -0600

    all styling finished

--- src/menu.js
+++ src/menu.js
@@ -4,18 +4,32 @@ const menuContent = () => {
         <h1>Bosco's Tacos</h1>
         <h3>Appetizers</h3>
         <h5>Little Tacos</h5>
-        <p>$8</p>
+        <h6>$8</h6>
         <h5>Taco Platter</h5>
-        <p>$11</p>
+        <h6>$11</h6>
         <h3>Entreés</h3>
         <h5>Bosco's Bozo Taco</h5>
-        <p>$6</p>
+        <h6>$6</h6>
         <h5>3 Tacos</h5>
-        <p>$12</p>
+        <h6>$12</h6>
         <h5>Big Tacos</h5>
-        <p>$16</p>
+        <h6>$16</h6>
         <h5>Large Taco Platter</h5>
-        <p>$20</p>
+        <h6>$20</h6>
+        <h3>Appetizers</h3>
+        <h5>Little Tacos</h5>
+        <h6>$8</h6>
+        <h5>Taco Platter</h5>
+        <h6>$11</h6>
+        <h3>Entreés</h3>
+        <h5>Bosco's Bozo Taco</h5>
+        <h6>$6</h6>
+        <h5>3 Tacos</h5>
+        <h6>$12</h6>
+        <h5>Big Tacos</h5>
+        <h6>$16</h6>
+        <h5>Large Taco Platter</h5>
+        <h6>$20</h6>
     `
     return a;
 }
